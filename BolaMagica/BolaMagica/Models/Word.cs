25c5a556c3ccaaf72eebc3c511256aa069c88f72
Sergio <a2019112566@estudiantes.upsa.edu.bo>
2021-02-04 10:09:30 -0400

    Commit Bola Magica

---
+++ BolaMagica/BolaMagica/Models/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BolaMagica.Models
+{
+    public class Word
+    {
+        [Key]
+        [Required]
+        public int WordId { get; set; }
+        [Required]
+        public string WordMessage { get; set; }
+    }
+}
